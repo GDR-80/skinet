f1136c37bede8e8500220b717f313ee157222d89
Garon <garonross@hotmail.co.uk>
2020-05-20 19:59:13 +0100

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -18,12 +20,12 @@ namespace API
 
     }
 
-    public IConfiguration Configuration { get; }
-
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddScoped<IProductRepository, ProductRepository>();
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+      services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x =>
        x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
@@ -40,6 +42,7 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+      app.UseStaticFiles();
 
       app.UseAuthorization();
 
