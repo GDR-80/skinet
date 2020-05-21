2a1b3b02233c3f0e29fe7e8191c27bd8af4f8dbf
Garon <garonross@hotmail.co.uk>
2020-05-21 21:11:05 +0100

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,12 +9,13 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+
+  public class ProductsController : BaseApiController
   {
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<Product> _productsRepo;
@@ -23,7 +24,7 @@ namespace API.Controllers
 
     public ProductsController(IGenericRepository<Product> productsRepo,
                               IGenericRepository<ProductBrand> productBrandRepo,
-                              IGenericRepository<ProductType> productTypeRepo, 
+                              IGenericRepository<ProductType> productTypeRepo,
                               IMapper mapper)
     {
       _productsRepo = productsRepo;
@@ -38,16 +39,21 @@ namespace API.Controllers
       var spec = new ProductsWithTypesAndBrandsSpecification();
       var products = await _productsRepo.ListAsync(spec);
 
-     return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+      return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
 
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
       var product = await _productsRepo.GetEntityWithSpec(spec);
+
+      if (product == null) return NotFound(new ApiResponse(404));
+
       return _mapper.Map<Product, ProductToReturnDto>(product);
 
     }
